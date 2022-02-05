da9a3e7c8557ceb6e2132ade065a5b417b6b9a48
Lucas Dreher <lucasdreher@gmail.com>
2022-02-05 09:48:23 +0100

    add to this folder

---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,18 @@
+import { useParams } from 'react-router-dom';
+
+import
+
+const QuoteDetail = () => {
+	const params = useParams();
+
+	console.log(params);
+
+	return (
+		<section>
+			<h1>QuoteDetail</h1>
+			<p>{params.quoteId}</p>
+		</section>
+	);
+};
+
+export default QuoteDetail;
