b590ef5a51cde7433ca6c7a9b7967dafe9cfeee1
Lucas Dreher <lucasdreher@gmail.com>
2022-02-05 17:47:32 +0100

    Sending & Getting Quote Data via Http

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,65 +1,40 @@
+import { useEffect } from 'react';
 import { Link, Route, Switch, useParams, useRouteMatch } from 'react-router-dom';
 
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
-
-const DUMMY_QUOTES = [
-	{
-		id: 'q1',
-		author: 'Nelson Mandela',
-		text: 'The greatest glory in living lies not in never falling, but in rising every time we fall.'
-	},
-	{
-		id: 'q2',
-		author: 'Walt Disney',
-		text: 'The way to get started is to quit talking and begin doing.'
-	},
-	{
-		id: 'q3',
-		author: 'Steve Jobs',
-		text:
-			"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking."
-	},
-	{
-		id: 'q4',
-		author: 'Eleanor Roosevelt',
-		text: 'If life were predictable it would cease to be life, and be without flavor.'
-	},
-	{
-		id: 'q5',
-		author: 'Oprah Winfrey',
-		text:
-			"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough."
-	},
-	{
-		id: 'q6',
-		author: 'James Cameron',
-		text: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success."
-	},
-	{
-		id: 'q7',
-		author: 'John Lennon',
-		text: "Life is what happens when you're busy making other plans."
-	},
-	{
-		id: 'q8',
-		author: 'Max',
-		text: 'Learning React is fun!'
-	},
-	{
-		id: 'q9',
-		author: 'Maximilian',
-		text: 'Learning React is great!'
-	}
-];
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import useHttp from '../hooks/use-http';
+import { getSingleQuote } from '../lib/api';
 
 const QuoteDetail = () => {
 	const match = useRouteMatch();
 	const params = useParams();
 
-	const quote = DUMMY_QUOTES.find((item) => item.id === params.quoteId);
+	const { quoteId } = params;
 
-	if (!quote) {
+	const { sendRequest, status, data: loadedQuote, error } = useHttp(getSingleQuote, true);
+
+	useEffect(
+		() => {
+			sendRequest(quoteId);
+		},
+		[ sendRequest, quoteId ]
+	);
+
+	if (status === 'pending') {
+		return (
+			<div className="centered">
+				<LoadingSpinner />
+			</div>
+		);
+	}
+
+	if (error) {
+		return <p className="centered focused">{error}</p>;
+	}
+
+	if (!loadedQuote) {
 		return (
 			<div className="centered">
 				<p>No quote found!</p>
@@ -69,7 +44,7 @@ const QuoteDetail = () => {
 
 	return (
 		<section>
-			<HighlightedQuote text={quote.text} author={quote.author} />
+			<HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
 			<Switch>
 				<Route path={`${match.path}`} exact>
 					<div className="centered">
