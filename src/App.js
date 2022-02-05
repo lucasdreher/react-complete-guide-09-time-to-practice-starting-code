bafe279247fa8d1b902e042ef2e0e678a960a620
Lucas Dreher <lucasdreher@gmail.com>
2022-02-05 10:18:16 +0100

    Adding a Layout Wrapper Component

--- src/App.js
+++ src/App.js
@@ -6,31 +6,29 @@ import Welcome from './pages/Welcome';
 import Quotes from './pages/Quotes';
 import QuoteDetail from './pages/QuoteDetail';
 import NewQuote from './pages/NewQuote';
+import Layout from './components/layout/Layout';
 
 function App() {
 	return (
-		<Fragment>
-			<MainNavigation />
-			<main>
-				<Switch>
-					<Route path="/" exact>
-						<Redirect to="welcome" />
-					</Route>
-					<Route path="/welcome">
-						<Welcome />
-					</Route>
-					<Route path="/quotes" exact>
-						<Quotes />
-					</Route>
-					<Route path="/quotes/:quoteId">
-						<QuoteDetail />
-					</Route>
-					<Route path="/new-quote">
-						<NewQuote />
-					</Route>
-				</Switch>
-			</main>
-		</Fragment>
+		<Layout>
+			<Switch>
+				<Route path="/" exact>
+					<Redirect to="welcome" />
+				</Route>
+				<Route path="/welcome">
+					<Welcome />
+				</Route>
+				<Route path="/quotes" exact>
+					<Quotes />
+				</Route>
+				<Route path="/quotes/:quoteId">
+					<QuoteDetail />
+				</Route>
+				<Route path="/new-quote">
+					<NewQuote />
+				</Route>
+			</Switch>
+		</Layout>
 	);
 }
 
